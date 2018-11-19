40b9f3cc79a7e8dd010112f1613e894e02e87152
Nathan Hosselton <nathanhosselton@gmail.com>
2018-11-18 22:45:46 -0500

    Implement trivial test for full API stack Not a good unit test but should at least catch almost everything for now.

---
+++ DadKitTests/Bungie.swift
@@ -0,0 +1,37 @@
+import XCTest
+@testable import DadKit
+import PMKFoundation
+
+class BungieTests: XCTestCase {
+
+    override func setUp() {
+        Bungie.key = ENV_API_KEY
+        Bungie.appId = ""
+        Bungie.appVersion = ""
+    }
+
+    func test_FullRequestPathSucceeds() {
+        let x = expectation(description: #file + #function)
+        
+
+        firstly {
+            Bungie.searchForClan(named: "Meow Pew Pew")
+        }.then {
+            Bungie.getClanMembers(in: $0)
+        }.map {
+            $0.prefix(6)
+        }.mapValues {
+            Bungie.getCurrentCharacter(for: $0.destinyUserInfo)
+        }.then {
+            when(fulfilled: $0)
+        }.done { _ in
+            x.fulfill()
+        }.catch {
+            print($0)
+            XCTFail($0.localizedDescription)
+            x.fulfill()
+        }
+
+        wait(for: [x], timeout: 1000)
+    }
+}
